1660d4d34d474598d93e066879bb70f9edaac37b
Dhanashree-98 <dhanashreegaikwad621998@gmail.com>
2025-04-24 12:09:07 +0530

    Update customers_report.sql

--- customers_report.sql
+++ customers_report.sql
@@ -26,8 +26,13 @@ Highlights:
 -- Create Report: gold.report_customers
 -- =============================================================================
 
-CREATE VIEW gold.report_customers AS
 
+IF OBJECT_ID('gold.report_customers', 'V') IS NOT NULL
+    DROP VIEW gold.report_customers;
+GO
+;
+
+CREATE VIEW gold.report_customers AS
 
 WITH base_query AS(
 
@@ -36,15 +41,15 @@ WITH base_query AS(
 ---------------------------------------------------------------------------*/
 
 SELECT
-f.order_number,
-f.product_key,
-f.order_date,
-f.sales_amount,
-f.quantity,
-c.customer_key,
-c.customer_number,
-CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
-DATEDIFF(YEAR, c.birthdate, GETDATE()) AS age
+	f.order_number,
+	f.product_key,
+	f.order_date,
+	f.sales_amount,
+	f.quantity,
+	c.customer_key,
+	c.customer_number,
+	CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+	DATEDIFF(YEAR, c.birthdate, GETDATE()) AS age
 FROM gold.fact_sales f
 LEFT JOIN
 gold.dim_customers c
@@ -60,59 +65,59 @@ WHERE order_date IS NOT NULL
 ---------------------------------------------------------------------------*/
 
 SELECT
-customer_key,
-customer_number,
-customer_name,
-age,
-COUNT(DISTINCT order_number) AS total_orders,
-SUM(sales_amount) AS total_sales,
-SUM(quantity) AS total_quantity,
-COUNT(DISTINCT product_key) AS total_products,
-MAX(order_date) AS last_order_date,
-DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan
+	customer_key,
+	customer_number,
+	customer_name,
+	age,
+	COUNT(DISTINCT order_number) AS total_orders,
+	SUM(sales_amount) AS total_sales,
+	SUM(quantity) AS total_quantity,
+	COUNT(DISTINCT product_key) AS total_products,
+	MAX(order_date) AS last_order_date,
+	DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan
 FROM base_query
 GROUP BY
-customer_key,
-customer_number,
-customer_name,
-age
+	customer_key,
+	customer_number,
+	customer_name,
+	age
 )
 
 
 SELECT
-customer_key,
-customer_number,
-customer_name,
-age,
+	customer_key,
+	customer_number,
+	customer_name,
+	age,
 CASE 
-WHEN age < 30 THEN 'Under 30'
-WHEN age BETWEEN 30 AND 39 THEN '30 - 39'
-WHEN age BETWEEN 40 AND 49 THEN '40 - 49'
-WHEN age BETWEEN 50 AND 59 THEN '50 - 59'
-ELSE '60 and above'
+	WHEN age < 30 THEN 'Under 30'
+	WHEN age BETWEEN 30 AND 39 THEN '30 - 39'
+	WHEN age BETWEEN 40 AND 49 THEN '40 - 49'
+	WHEN age BETWEEN 50 AND 59 THEN '50 - 59'
+	ELSE '60 and above'
 END AS age_group,
 CASE
-WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
-WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
-ELSE 'New'
+	WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
+	WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
+	ELSE 'New'
 END customer_segment,
-DATEDIFF(MONTH, last_order_date, GETDATE()) AS recency,
-total_orders,
-total_sales,
-total_quantity,
-total_products,
-last_order_date,
-lifespan,
+	DATEDIFF(MONTH, last_order_date, GETDATE()) AS recency,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_products,
+	last_order_date,
+	lifespan,
 -- Compute average order value (AVO)
-CASE WHEN total_orders = 0 THEN 0
-     ELSE total_sales/total_orders 
-END AS avg_order_value,
+	CASE WHEN total_orders = 0 THEN 0
+	     ELSE total_sales/total_orders 
+	END AS avg_order_value,
 
 -- Compute average monthly spend
-CASE WHEN lifespan = 0 THEN total_sales
-     ELSE total_sales/lifespan
-END AS avg_monthly_spend
-FROM customer_aggregation
+	CASE WHEN lifespan = 0 THEN total_sales
+	     ELSE total_sales/lifespan
+	END AS avg_monthly_spend
+FROM customer_aggregation;
 
 
 
